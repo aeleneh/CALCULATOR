d5116e6a03981690bfc26fe6ae02a7001811c743
Enahoroomowunmi@hmail.com <69280538+Enahoro97@users.noreply.github.com>
2022-06-23 19:02:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let x,y;
+x = parseFloat(parseInt(prompt("enter the first number")));
+y = parseFloat(parseInt(prompt("enter the second number")));
+
+let z = x+y;
+let a = x-y;
+let b = x*y;
+let c = x/y;
+
+alert("the sum of the given numbers is:" +z);
+alert("the difference of the given numbers is:" +a);
+alert("the product of the given numbers is:" +b);
+alert("the quoient of the given numbers is:" +c);
+
